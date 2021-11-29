20d54552449038ac0e0b509a2dca40ba92169903
Taek713 <tae.kim147@gmail.com>
2021-11-29 16:07:28 -0500

    Hope.

---
+++ src/components/header.js
@@ -0,0 +1,63 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import $ from 'jquery';
+
+
+
+// portfolio
+$('.gallery ul li a').click(function() {
+  var itemID = $(this).attr('href');
+  $('.gallery ul').addClass('item_open');
+  $(itemID).addClass('item_open');
+  return false;
+});
+$('.close').click(function() {
+  $('.port, .gallery ul').removeClass('item_open');
+  return false;
+});
+
+$(".gallery ul li a").click(function() {
+  $('html, body').animate({
+      scrollTop: parseInt($("#top").offset().top)
+  }, 400);
+});
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `black`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,124 @@
+// import React from "react"
+
+// export default function Home() {
+//   return <div>Hello world!</div>
+// }
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <div className="container">
+  <h1>Parallax</h1>
+  <h2>faster</h2>
+  <h2>easier</h2>
+  <h2>sassier</h2>
+  <img src="http://placekitten.com/200/200" alt=" " />
+  <img src="http://placekitten.com/150/200" alt=" " />
+  <img src="http://placekitten.com/200/150" alt=" " />
+  <img src="http://placekitten.com/150/250" alt=" " />
+  <img src="http://placekitten.com/300/400" alt=" " />
+  <img src="http://placekitten.com/400/300" alt=" " />
+</div>
+
+
+<div>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+  <link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet" type="text/css" />
+  <div id="top" />
+  <section className="gallery">
+    <div className="row">
+      <ul>
+        <a href="#" className="close" />
+        <li>
+          <a href="#item02">
+            <img src="https://cdn.dribbble.com/users/545884/screenshots/3981307/lorena2.png" alt />
+          </a>
+        </li>
+        <li>
+          <a href="#item02">
+            <img src="https://cdn.dribbble.com/users/545884/screenshots/3892302/contact.png" alt />
+          </a>
+        </li>
+        <li>
+          <a href="#item02">
+            <img src="https://cdn.dribbble.com/users/545884/screenshots/4154721/dive--001.png" alt />
+          </a>
+        </li>
+        <li>
+          <a className="image" href="#item01">
+            <img src="https://cdn.dribbble.com/users/545884/screenshots/4356121/darko--dr.jpg" alt />
+          </a>
+        </li>
+        <li>
+          <a className="image" href="#item02">
+            <img src="https://cdn.dribbble.com/users/545884/screenshots/3695553/news.png" alt />
+          </a>
+        </li>
+        <li>
+          <a className="image" href="#item01">
+            <img src="https://cdn.dribbble.com/users/545884/screenshots/4360101/liberosis_--_12.png" alt />
+          </a>
+        </li>
+        <li>
+          <a className="image" href="#item01">
+            <img src="https://cdn.dribbble.com/users/545884/screenshots/3148799/technical-book.jpg" alt />
+          </a>
+        </li>
+     
+      </ul>
+    </div> 
+    
+    <div id="item01" className="port">
+      <div className="row">
+        <div className="description">
+          <h1>Item 01</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis libero erat. Integer ac purus est. Proin erat mi, pulvinar ut magna eget, consectetur auctor turpis.</p>
+        </div>
+        <img src="https://cdn.dribbble.com/users/545884/screenshots/3981307/lorena2.png" alt />
+      </div>
+    </div> 
+    
+    
+    <div id="item02" className="port">
+      <div className="row">
+        <div className="description">
+          <h1>Item 02</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis libero erat. Integer ac purus est. Proin erat mi, pulvinar ut magna eget, consectetur auctor turpis.</p>
+        </div>
+        <img src="https://cdn.dribbble.com/users/545884/screenshots/2883479/cover.jpg" alt />
+      </div> 
+    </div> 
+  </section> 
+</div>
+
+<div>
+  <h1>
+    <em>Theres Never any</em>
+    <span><b aria-hidden="true"><i>~</i><i>~</i><i>~</i></b> Competition <b aria-hidden="true"><i>~</i><i>~</i><i>~</i></b></span>
+  </h1>
+  <div className="content">
+  <div>
+  <p>Mrs. Dalloway said she would buy the flowers herself.</p>
+  <p>For Lucy had her work cut out for her. The doors would be taken off their hinges; Rumpelmayer's men were coming. And then, thought Clarissa Dalloway, what a morning--fresh as if issued to children on a beach.</p>
+  <p>What a lark! What a plunge! For so it had always seemed to her, when, with a little squeak of the hinges, which she could hear now, she had burst open the French windows and plunged at Bourton into the open air. How fresh, how calm, stiller than this of course, the air was in the early morning; like the flap of a wave; the kiss of a wave; chill and sharp and yet (for a girl of eighteen as she then was) solemn, feeling as she did, standing there at the open window, that something awful was about to happen; looking at the flowers, at the trees with the smoke winding off them and the rooks rising, falling; standing and looking until Peter Walsh said, "Musing among the vegetables?"--was that it?--"I prefer men to cauliflowers"--was that it? He must have said it at breakfast one morning when she had gone out on to the terrace--Peter Walsh. He would be back from India one of these days, June or July, she forgot which, for his letters were awfully dull; it was his sayings one remembered; his eyes, his pocket-knife, his smile, his grumpiness and, when millions of things had utterly vanished--how strange it was!--a few sayings like this about cabbages.</p>
+  <p>She stiffened a little on the kerb, waiting for Durtnall's van to pass. A charming woman, Scrope Purvis thought her (knowing her as one does know people who live next door to one in Westminster); a touch of the bird about her, of the jay, blue-green, light, vivacious, though she was over fifty, and grown very white since her illness. There she perched, never seeing him, waiting to cross, very upright.</p>
+  <p>For having lived in Westminster--how many years now? over twenty,--one feels even in the midst of the traffic, or waking at night, Clarissa was positive, a particular hush, or solemnity; an indescribable pause; a suspense (but that might be her heart, affected, they said, by influenza) before Big Ben strikes. There! Out it boomed. First a warning, musical; then the hour, irrevocable. The leaden circles dissolved in the air. Such fools we are, she thought, crossing Victoria Street. For Heaven only knows why one loves it so, how one sees it so, making it up, building it round one, tumbling it, creating it every moment afresh; but the veriest frumps, the most dejected of miseries sitting on doorsteps (drink their downfall) do the same; can't be dealt with, she felt positive, by Acts of Parliament for that very reason: they love life. In people's eyes, in the swing, tramp, and trudge; in the bellow and the uproar; the carriages, motor cars, omnibuses, vans, sandwich men shuffling and swinging; brass bands; barrel organs; in the triumph and the jingle and the strange high singing of some aeroplane overhead was what she loved; life; London; this moment of June.</p>
+  <p>For it was the middle of June. The War was over, except for some one like Mrs. Foxcroft at the Embassy last night eating her heart out because that nice boy was killed and now the old Manor House must go to a cousin; or Lady Bexborough who opened a bazaar, they said, with the telegram in her hand, John, her favourite, killed; but it was over; thank Heaven--over. It was June. The King and Queen were at the Palace. And everywhere, though it was still so early, there was a beating, a stirring of galloping ponies, tapping of cricket bats; Lords, Ascot, Ranelagh and all the rest of it; wrapped in the soft mesh of the grey-blue morning air, which, as the day wore on, would unwind them, and set down on their lawns and pitches the bouncing ponies, whose forefeet just struck the ground and up they sprung, the whirling young men, and laughing girls in their transparent muslins who, even now, after dancing all night, were taking their absurd woolly dogs for a run; and even now, at this hour, discreet old dowagers were shooting out in their motor cars on errands of mystery; and the shopkeepers were fidgeting in their windows with their paste and diamonds, their lovely old sea-green brooches in eighteenth-century settings to tempt Americans (but one must economise, not buy things rashly for Elizabeth), and she, too, loving it as she did with an absurd and faithful passion, being part of it, since her people were courtiers once in the time of the Georges, she, too, was going that very night to kindle and illuminate; to give her party. But how strange, on entering the Park, the silence; the mist; the hum; the slow-swimming happy ducks; the pouched birds waddling; and who should be coming along with his back against the Government buildings, most appropriately, carrying a despatch box stamped with the Royal Arms, who but Hugh Whitbread; her old friend Hugh--the admirable Hugh!</p>
+  <p>"Good-morning to you, Clarissa!" said Hugh, rather extravagantly, for they had known each other as children. "Where are you off to?"</p>
+  <p>"I love walking in London," said Mrs. Dalloway. "Really it's better than walking in the country."</p>
+</div>
+
+    
+    <p>Grab me on Twitter (<a href="https://twitter.com/Seoulfed">@Seoulfed</a>) if you have any questions about how I made it</p>
+  </div>
+</div>
+
+  </Layout>
+)
+
+export default IndexPage
