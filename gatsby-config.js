88a5d460446176c3a323ea8dfcaa1b442e7945d1
Taek713 <tae.kim147@gmail.com>
2021-11-30 09:36:44 -0500

    hopefully error gone

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,10 +25,10 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // https://css-tricks.com/meta-theme-color-npm install gatsby-plugin-manifestand-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/logo3.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
